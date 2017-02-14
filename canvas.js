05bf2d306dcc186fe64eabca0782e41aea016d97
achencoms <achencoms@gmail.com>
2017-02-13 21:50:18 -0500

    I have tasted the meats

--- canvas.js
+++ canvas.js
@@ -1,7 +1,12 @@
 var myCanvas = document.getElementById("picasso");
 var ctx = myCanvas.getContext("2d");
+var clear = document.getElementById("clear");
 
-myCanvas.onclick = function(e){
+myCanvas.onmousedown = function(e){
 	ctx.fillStyle = "#ff00ff";
-	ctx.fillRect(e.clientX,e.clientY,e.clientX+100,e.clientY+100);
+	ctx.fillRect(e.clientX,e.clientY,50,50);
 };
+
+clear.onclick = function(){
+	ctx.clearRect(0,0,400,400)
+}
